4d4843edfda9826c14c729cce6856581e5922a2f
hyeonjin <hjine01@naver.com>
2024-11-14 12:17:45 +0900

    ios

--- BedrockApp/ios/BedrockApp/AppDelegate.mm
+++ BedrockApp/ios/BedrockApp/AppDelegate.mm
@@ -1,17 +1,26 @@
 #import "AppDelegate.h"
 
+#import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"BedrockApp";
-  // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
+  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
+                                                   moduleName:@"BedrockApp"
+                                            initialProperties:nil];
 
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+  rootView.backgroundColor = [UIColor colorWithRed:(15.0/255.0) green:(15.0/255.0) blue:(15.0/255.0) alpha:1.0];
+  
+  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  UIViewController *rootViewController = [UIViewController new];
+  rootViewController.view = rootView;
+  self.window.rootViewController = rootViewController;
+  [self.window makeKeyAndVisible];
+  return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
@@ -21,11 +30,9 @@
 
 - (NSURL *)bundleURL
 {
-#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-#else
-  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-#endif
+  NSURL *releaseURL = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  NSLog(@"RELEASE bundle URL: %@", releaseURL);
+  return releaseURL;
 }
 
 @end
